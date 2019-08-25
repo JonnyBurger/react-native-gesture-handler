b7640168be0c3a1cd2924fa142da7dd40381d99a
Jonny Burger <jonathanburger11@gmail.com>
2019-08-25 21:10:34 +0200

    use animated api

--- touchables/TouchableHighlight.js
+++ touchables/TouchableHighlight.js
@@ -1,101 +1,107 @@
 import React, { Component } from 'react';
 import GenericTouchable, { TOUCHABLE_STATE } from './GenericTouchable';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Animated } from 'react-native';
 import PropTypes from 'prop-types';
 
 /**
  * TouchableHighlight follows RN's implementation
  */
 export default class TouchableHighlight extends Component {
-  static defaultProps = {
-    ...GenericTouchable.defaultProps,
-    activeOpacity: 0.85,
-    delayPressOut: 100,
-    underlayColor: 'black',
-  };
+	static defaultProps = {
+		...GenericTouchable.defaultProps,
+		activeOpacity: 0.85,
+		delayPressOut: 100,
+		underlayColor: 'black'
+	};
 
-  static propTypes = {
-    ...GenericTouchable.publicPropTypes,
-    activeOpacity: PropTypes.number,
-    underlayColor: PropTypes.string,
-    style: PropTypes.any,
-    onShowUnderlay: PropTypes.func,
-    onHideUnderlay: PropTypes.func,
-  };
+	static propTypes = {
+		...GenericTouchable.publicPropTypes,
+		activeOpacity: PropTypes.number,
+		underlayColor: PropTypes.string,
+		style: PropTypes.any,
+		onShowUnderlay: PropTypes.func,
+		onHideUnderlay: PropTypes.func
+	};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      extraChildStyle: null,
-      extraUnderlayStyle: null,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			extraChildStyle: null,
+			extraUnderlayStyle: null
+		};
+	}
 
-  // Copied from RN
-  showUnderlay = () => {
-    if (!this.hasPressHandler()) {
-      return;
-    }
-    this.setState({
-      extraChildStyle: {
-        opacity: this.props.activeOpacity,
-      },
-      extraUnderlayStyle: {
-        backgroundColor: this.props.underlayColor,
-      },
-    });
-    this.props.onShowUnderlay && this.props.onShowUnderlay();
-  };
+	pressedIn = new Animated.Value(0);
 
-  hasPressHandler = () =>
-    this.props.onPress ||
-    this.props.onPressIn ||
-    this.props.onPressOut ||
-    this.props.onLongPress;
+	// Copied from RN
+	showUnderlay = () => {
+		if (!this.hasPressHandler()) {
+			return;
+		}
+		Animated.timing(this.pressedIn, {
+			toValue: 1,
+			duration: 0,
+			useNativeDriver: true,
+		}).start();
+		this.props.onShowUnderlay && this.props.onShowUnderlay();
+	};
 
-  hideUnderlay = () => {
-    this.setState({
-      extraChildStyle: null,
-      extraUnderlayStyle: null,
-    });
-    this.props.onHideUnderlay && this.props.onHideUnderlay();
-  };
+	hasPressHandler = () =>
+		this.props.onPress ||
+		this.props.onPressIn ||
+		this.props.onPressOut ||
+		this.props.onLongPress;
 
-  renderChildren() {
-    if (!this.props.children) {
-      return <View />;
-    }
+	hideUnderlay = () => {
+		Animated.timing(this.pressedIn, {
+			toValue: 0,
+			duration: 0,
+			useNativeDriver: true,
+		}).start();
+		this.props.onHideUnderlay && this.props.onHideUnderlay();
+	};
 
-    const child = React.Children.only(this.props.children);
-    return React.cloneElement(child, {
-      style: StyleSheet.compose(
-        child.props.style,
-        this.state.extraChildStyle
-      ),
-    });
-  }
+	renderChildren() {
+		if (!this.props.children) {
+			return <View />;
+		}
 
-  onStateChange = (from, to) => {
-    if (to === TOUCHABLE_STATE.BEGAN) {
-      this.showUnderlay();
-    } else if (
-      to === TOUCHABLE_STATE.UNDETERMINED ||
-      to === TOUCHABLE_STATE.MOVED_OUTSIDE
-    ) {
-      this.hideUnderlay();
-    }
-  };
+		const child = React.Children.only(this.props.children);
+		return React.cloneElement(child, {
+			style: StyleSheet.compose(
+				child.props.style,
+				this.state.extraChildStyle
+			)
+		});
+	}
 
-  render() {
-    const { style = {}, ...rest } = this.props;
-    const { extraUnderlayStyle } = this.state;
-    return (
-      <GenericTouchable
-        {...rest}
-        style={[style, extraUnderlayStyle]}
-        onStateChange={this.onStateChange}>
-        {this.renderChildren()}
-      </GenericTouchable>
-    );
-  }
+	onStateChange = (from, to) => {
+		if (to === TOUCHABLE_STATE.BEGAN) {
+			this.showUnderlay();
+		} else if (
+			to === TOUCHABLE_STATE.UNDETERMINED ||
+			to === TOUCHABLE_STATE.MOVED_OUTSIDE
+		) {
+			this.hideUnderlay();
+		}
+	};
+
+	render() {
+		const { style = {}, ...rest } = this.props;
+		const { extraUnderlayStyle } = this.state;
+		return (
+			<GenericTouchable
+				{...rest}
+				style={[style, {
+					backgroundColor: this.pressedIn.interpolate({
+						inputRange: [0, 1],
+						outputRange: ['rgba(255, 255, 255, 0)', this.props.underlayColor]
+					})
+				}]}
+				onStateChange={this.onStateChange}
+			>
+				{this.renderChildren()}
+			</GenericTouchable>
+		);
+	}
 }
